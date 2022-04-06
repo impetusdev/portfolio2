6cbcea059412c04bc03757945544ca13f6c1899e
Laurence Qi <laurence.qi@outlook.com>
2022-04-06 16:39:30 +1000

    added projects section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Banner from './components/Banner';
 import Nav from './components/Nav';
 import Section from './components/Section';
+import Projects from './components/Projects';
 import './App.css';
 
 function App() {
@@ -8,7 +9,8 @@ function App() {
     <div className="App">
       
       <Banner></Banner>
-      <Section ></Section>
+      <Section></Section>
+      <Projects></Projects>
       <Nav></Nav>
     </div>
   );
---
+++ src/components/Projects.jsx
@@ -0,0 +1,16 @@
+import './Section.scss';
+// import { ReactComponent as YourSvg } from 'public/skills/c-sharp.svg';
+// import { ReactComponent as IconMenu } from '/c-sharp.svg'
+
+export default function Section(props){
+    function importAll(r) {
+        return r.keys().map(r);
+    }
+      
+    return <div className='section'>
+        <div className='container'>
+            <h1 className='title'>Projects</h1>
+            
+        </div>
+    </div>
+}
