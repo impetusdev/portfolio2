47979a986c7bb3b3075edd2a4ecf56bd2ce36c83
Laurence Qi <laurence.qi@outlook.com>
2022-04-08 08:01:16 +1000

    added video

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,7 +1,43 @@
+import { useRef, useEffect } from 'react';
+import { gsap } from "gsap";
+import { ScrollTrigger } from 'gsap/all';
+
 import './Banner.scss';
 
 export default function Banner(props) {
-    return <div className='Banner'>
+
+    const IntroVideoRef = useRef(null);
+    const videoRef = useRef(null);
+  
+    useEffect(() => {
+        gsap.registerPlugin(ScrollTrigger);
+        
+        videoRef.current.currentTime = 0;
+        
+        ScrollTrigger.create({
+            trigger: IntroVideoRef.current,
+            scrub: true,
+            pin: IntroVideoRef.current,
+            start: 'center center',
+            end: '+=20000',
+            onUpdate: function(self) { 
+                if(videoRef.current) {
+                    let scrollPos = self.progress;
+                    let videoDuration = videoRef.current.duration;
+                    let videoCurrentTime = videoDuration + scrollPos;
+                    
+                    if(videoCurrentTime) {
+                        videoRef.current.currentTime = videoCurrentTime;
+                    }
+                    
+                    console.log(videoDuration, scrollPos, videoCurrentTime)
+                }
+            },
+        })
+    }, [IntroVideoRef, videoRef]);
+
+    return <div className='Banner' ref={IntroVideoRef}>
+        <video id="video" src="starry.mp4" ref={videoRef}></video>
         <img id='profile' src='profile.png' alt='Laurence Qi'/>
         <div id='blur'></div>
         <h1 id='name'>Laurence Qi</h1>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,8 +2,8 @@ import './Nav.scss';
 
 export default function Banner(props) {
     return <ul className='Nav'>
-        <li><a class='Nav' href='https://www.linkedin.com/in/laurence-qi-a4944513b/'>LinkedIn</a></li>
-        <li><a class='Nav' href='https://github.com/impetusdev'>GitHub</a></li>
-        <li><a class='Nav' href='https://twitter.com/life_is_a_model'>Twitter</a></li>
+        <li><a className='Nav' href='https://www.linkedin.com/in/laurence-qi-a4944513b/'>LinkedIn</a></li>
+        <li><a className='Nav' href='https://github.com/impetusdev'>GitHub</a></li>
+        <li><a className='Nav' href='https://twitter.com/life_is_a_model'>Twitter</a></li>
     </ul>
 }
