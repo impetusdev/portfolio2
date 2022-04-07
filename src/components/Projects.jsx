5ffbd5d13a8663ae8380cd424a82c6f13fbf6b22
Laurence Qi <laurence.qi@outlook.com>
2022-04-07 16:19:54 +1000

    changed background darkening

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,7 +7,7 @@ export default function Projects(props){
         <div className='container' id='projects'>
             <h1 className='title'>Projects</h1>
             <Project title='Metaverse Fighting Champioship' imgSrc='projects/mfc.png' description='A dog walking application that serves as a marketplace for both walkers and dog owners to connect and track their locations. Capable of using both real time & mocking the walker’s location data.'></Project>
-            <Project title='Unleashed' imgSrc='projects/unleashed.png' description =''></Project>
+            <Project title='Unleashed' imgSrc='projects/unleashed.png' description ='A dog walking application that serves as a marketplace for both walkers and dog owners to connect and track their locations. Capable of using both real time & mocking the walker’s location data. '></Project>
             <Project title='Rental-Assessor' imgSrc='' description ='An apartment valuation app that allows the user to calculate their weekly travel time and location safety.'></Project>
         </div>
     </div>
