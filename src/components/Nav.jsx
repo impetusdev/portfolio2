6fe66f6b7174e7fc88f6bfe05ebe3238e40a181d
Laurence Qi <laurence.qi@outlook.com>
2022-04-10 17:29:10 +1000

    changed names

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -19,9 +19,9 @@ export default function Banner(props) {
     //TODO: make these links the ones for the actual sections. 
     return <div>
         <ul className={offset > window.innerHeight ? 'sticky' : ''}>
-            <li><a href='https://www.linkedin.com/in/laurence-qi-a4944513b/'>LinkedIn</a></li>
-            <li><a href='https://github.com/impetusdev'>GitHub</a></li>
-            <li><a href='https://twitter.com/life_is_a_model'>Twitter</a></li>
+            <li><a href='https://www.linkedin.com/in/laurence-qi-a4944513b/'>About Me</a></li>
+            <li><a href='https://github.com/impetusdev'>Skills</a></li>
+            <li><a href='https://twitter.com/life_is_a_model'>Projects</a></li>
         </ul>
         <ul className={offset > window.innerHeight ? 'empty' : ''}><li></li></ul>
     </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,7 +3,7 @@ import './Skills.scss';
 import './Projects.scss';
 
 export default function Projects(props){
-    return <div className='projects'>
+    return <div className='section'>
         <div className='container' id='projects'>
             <h1 className='title'>Projects</h1>
             <Project title='Metaverse Fighting Champioship' imgSrc='projects/mfc.png' description='A dog walking application that serves as a marketplace for both walkers and dog owners to connect and track their locations. Capable of using both real time & mocking the walker’s location data.'></Project>
