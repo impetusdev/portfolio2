db159067d668eb9bb4c2dbd56e281f53aba3a889
Laurence Qi <laurence.qi@outlook.com>
2022-04-12 11:12:23 +1000

    removed rental

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,11 +1,15 @@
 import './Project.scss';
 
 
-export default function Project({title, imgSrc, description}){
+export default function Project({title, imgSrc, description, demo, repo}){
       
     return <div className='project'>
         <h3>{title}</h3>
         <img  src={imgSrc} alt="yo" />
         <section>{description}</section>
+        <div className='links'>
+            <a href={demo}>DEMO</a>
+            <a href={repo}>REPO</a>
+        </div>
     </div>
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,8 +6,8 @@ export default function Projects(props){
     return <div className='section'>
         <div className='container' id='projects'>
             <h1 className='title'>Projects</h1>
-            <Project title='Metaverse Fighting Champioship' imgSrc='projects/mfc.png' description='A dog walking application that serves as a marketplace for both walkers and dog owners to connect and track their locations. Capable of using both real time & mocking the walker’s location data.'></Project>
-            <Project title='Unleashed' imgSrc='projects/unleashed.png' description ='A dog walking application that serves as a marketplace for both walkers and dog owners to connect and track their locations. Capable of using both real time & mocking the walker’s location data. '></Project>
+            <Project title='Metaverse Fighting Champioship' imgSrc='projects/mfc.png' demo='https://metaverse-fc.netlify.app/' repo='https://github.com/thelorddoyle/mfc-front-end' description='A dog walking application that serves as a marketplace for both walkers and dog owners to connect and track their locations. Capable of using both real time & mocking the walker’s location data.'></Project>
+            <Project title='Unleashed' imgSrc='projects/unleashed.png' demo='https://unleashed-client.netlify.app/' repo='https://github.com/Jesus-fhz/unleashed-client' description ='A dog walking application that serves as a marketplace for both walkers and dog owners to connect and track their locations. Capable of using both real time & mocking the walker’s location data. '></Project>
             {/* <Project title='Rental-Assessor' imgSrc='' description ='An apartment valuation app that allows the user to calculate their weekly travel time and location safety.'></Project> */}
         </div>
     </div>
