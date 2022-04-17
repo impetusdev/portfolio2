9c8c3140e3add7184ab137f2e7af6c041d1e208f
Laurence Qi <laurence.qi@outlook.com>
2022-04-17 17:18:34 +1000

    fixed links being mixed up

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,8 +5,8 @@ export default function Footer(props){
         <h3>Connect With Me</h3>
         
         <div className='links'>
-            <a className='Nav' href='https://www.linkedin.com/in/laurence-qi-a4944513b/'><img src="reachout/Github.svg" alt="github" /></a>
-            <a className='Nav' href='https://github.com/impetusdev'><img src="reachout/LinkedIn.svg" alt="LinkedIn" /></a>
+            <a className='Nav' href='https://github.com/impetusdev'><img src="reachout/Github.svg" alt="github" /></a>
+            <a className='Nav' href='https://www.linkedin.com/in/laurence-qi-a4944513b/'><img src="reachout/LinkedIn.svg" alt="LinkedIn" /></a>
             <a className='Nav' href='https://twitter.com/life_is_a_model'><img src="reachout/Twitter.svg" alt="Twitter"/></a>
         </div>
     </div>
