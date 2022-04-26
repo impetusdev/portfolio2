ce3908fc4694b3a0b2d9d88a16497c03d2e1fd26
Laurence Qi <laurence.qi@outlook.com>
2022-04-26 20:10:54 +1000

    ran npm build

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,6 +5,6 @@ export default function Banner(props) {
         <img id='profile' src='profile.png' alt='Laurence Qi'/>
         <div id='blur'></div>
         <h1 id='name'>Laurence Qi</h1>
-        <h2 id='sub-heading'>Junior Back-end Developer</h2>
+        <h2 id='sub-heading'>Junior Back-End Developer</h2>
     </div>
 }
