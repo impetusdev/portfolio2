b5864df9ac1a56069a549183e0423f7e56ed362c
Laurence Qi <laurence.qi@outlook.com>
2022-05-21 12:27:45 +1000

    added rel="noreferrer"

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,9 +5,9 @@ export default function Footer(props){
         <h3>Connect With Me</h3>
         
         <div className='links'>
-            <a className='Nav' href='https://github.com/impetusdev' target="_blank"><img src="reachout/Github.svg" alt="github" /></a>
-            <a className='Nav' href='https://www.linkedin.com/in/laurence-qi-a4944513b/' target="_blank"><img src="reachout/LinkedIn.svg" alt="LinkedIn" /></a>
-            <a className='Nav' href='https://twitter.com/life_is_a_model' target="_blank"><img src="reachout/Twitter.svg" alt="Twitter"/></a>
+            <a className='Nav' href='https://github.com/impetusdev' target="_blank" rel="noreferrer"><img src="reachout/Github.svg" alt="github" /></a>
+            <a className='Nav' href='https://www.linkedin.com/in/laurence-qi-a4944513b/' target="_blank" rel="noreferrer"><img src="reachout/LinkedIn.svg" alt="LinkedIn" /></a>
+            <a className='Nav' href='https://twitter.com/life_is_a_model' target="_blank" rel="noreferrer"><img src="reachout/Twitter.svg" alt="Twitter"/></a>
         </div>
     </div>
 }
