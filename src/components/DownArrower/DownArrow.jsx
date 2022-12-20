2cb111cd6a1bf50e40bf55995389247dfeb53775
Laurence Qi <laurence.qi@outlook.com>
2022-12-20 15:59:05 +1100

    Added comment to DownArrow

--- src/components/DownArrower/DownArrow.jsx
+++ src/components/DownArrower/DownArrow.jsx
@@ -3,7 +3,7 @@ import "./DownArrow.scss";
 export function DownArrow() {
   return (
     <div class="downArrow bounce">
-      <img width="100" height="100" alt="" src="DownArrow.svg" />
+      <img width="100" height="100" alt="" src="DownArrow.svg" /> //fix this src link
     </div>
   );
 }
