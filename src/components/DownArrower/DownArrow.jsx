bab99662f7cd341594f6ca87400b16fb10f3059b
Laurence Qi <laurence.qi@outlook.com>
2022-12-20 15:58:39 +1100

    Added bouncing arrow component

---
+++ src/components/DownArrower/DownArrow.jsx
@@ -0,0 +1,9 @@
+import "./DownArrow.scss";
+
+export function DownArrow() {
+  return (
+    <div class="downArrow bounce">
+      <img width="100" height="100" alt="" src="DownArrow.svg" />
+    </div>
+  );
+}
