c4c3afe87506298e784eed952cc53d811d3134aa
Laurence Qi <laurence.qi@outlook.com>
2022-12-24 14:11:11 +1100

    removed comments

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,24 +1,36 @@
-import { useEffect, useState } from 'react';
-import './Nav.scss';
+import { useEffect, useState } from "react";
+import "./Nav.scss";
 
 export default function Banner(props) {
-    const [offset, setOffset] = useState(0);
-    
-    // whenever the user scrolls evaluate if the window Y position > nav bar position, if so change the nav to sticky. 
-    useEffect(() => {
-        const onScroll = () => setOffset(window.pageYOffset);
-        window.removeEventListener('scroll', onScroll);
-        window.addEventListener('scroll', onScroll, { passive: true });
-        return () => window.removeEventListener('scroll', onScroll);
-    }, []);
-    
-    return <div>
-        <ul className={offset > window.innerHeight ? 'sticky' : ''}>
-            <a href='#aboutMe' className='navLink'>About Me</a>
-            <a href='#skills' className='navLink'>Skills</a>
-            <a href='#projects' className='navLink'>Projects</a>
-            <a href='#footer' className='navLink' id='connect'>Connect</a>
-        </ul>
-        <ul className={offset > window.innerHeight ? 'empty' : ''}><li></li></ul>
+  const [offset, setOffset] = useState(0);
+
+  // whenever the user scrolls evaluate if the window Y position > nav bar position, if so change the nav to sticky.
+  useEffect(() => {
+    const onScroll = () => setOffset(window.pageYOffset);
+    window.removeEventListener("scroll", onScroll);
+    window.addEventListener("scroll", onScroll, { passive: true });
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
+  return (
+    <div>
+      <ul className={offset > window.innerHeight ? "sticky" : ""}>
+        <a href="#aboutMe" className="navLink">
+          About Me
+        </a>
+        <a href="#skills" className="navLink">
+          Skills
+        </a>
+        <a href="#projects" className="navLink">
+          Projects
+        </a>
+        <a href="#footer" className="navLink" id="connect">
+          Connect
+        </a>
+      </ul>
+      <ul className={offset > window.innerHeight ? "empty" : ""}>
+        <li></li>
+      </ul>
     </div>
+  );
 }
