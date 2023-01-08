6f1f0f6294374752ca130caec6b82da0ff872154
Laurence Qi <laurence.qi@outlook.com>
2023-01-09 08:20:10 +1100

    removed stats

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { Banner } from "./components/Banner/Banner";
 import { Nav } from "./components/Nav/Nav";
 import { Skills } from "./components/Skills/Skills";
 import { AboutMe } from "./components/AboutMe/AboutMe";
-import { Stats } from "./components/Stats/Stats";
 import { Projects } from "./components/Projects/Projects";
 import "./App.css";
 import { Footer } from "./components/Footer/Footer";
@@ -15,7 +14,6 @@ function App() {
       <AboutMe></AboutMe>
       <Skills></Skills>
       <Projects></Projects>
-      {/* <Stats></Stats> */}
 
       <Footer></Footer>
     </div>
